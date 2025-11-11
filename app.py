bdfbda4d350b36e623596a59b3485920bba0072a
felipem-svg <felipem@mnply.com.br>
2025-11-11 17:43:05 -0300

    app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ CB_VIP_EXPLICAR = "vip_explicar"
 CB_VIP_PRINT = "vip_print"
 CB_VIP_DEPOSITAR = "vip_depositar"
 
-WAIT_SECONDS = 5
+WAIT_SECONDS = 60
 VIP_WAIT_SECONDS = 7 * 60
 VIP_PENDING_PRINT: set[int] = set()   # chats aguardando print
 AUDIO_FILE_LOCAL = "Audio.mp3"
